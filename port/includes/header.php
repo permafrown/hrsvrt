4942426d0715fd1ecf8949df59cbd3a928539266
permafrown <bjoelh@gmail.com>
2018-08-02 13:27:24 -0400

    port updates

--- port/includes/header.php
+++ port/includes/header.php
@@ -11,7 +11,20 @@
     <title>portfolio | hrsvrt.ca</title>
   </head>
 <body>
-  <div id="header_div" class="container-fluid">
-    <h1>portfolio<i class="fas fa-circle-notch"></i>hrsvrt.ca</h1>
-    <h2>take a look at my awesome portfolio of fun stuffs!</h2>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">portfolio<i class="fas fa-circle-notch"></i>hrsvrt.ca</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
   </div>
+</nav>
