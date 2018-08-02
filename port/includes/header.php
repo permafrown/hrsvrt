550c37d14097ed968eed2ae919d982cd4285217c
permafrown <bjoelh@gmail.com>
2018-08-02 12:19:50 -0400

    port test

--- port/includes/header.php
+++ port/includes/header.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap v4.1.3 -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Main Custom CSS -->
-      <link rel="stylesheet" href="/css/master.css">
+      <link rel="stylesheet" href="css/master.css">
     <title>portfolio | hrsvrt.ca</title>
   </head>
 <body>
