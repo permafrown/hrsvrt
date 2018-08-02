6129c061fc53327105e59600c3946627011c6378
permafrown <bjoelh@gmail.com>
2018-08-02 13:42:59 -0400

    port updates

--- port/index.php
+++ port/index.php
@@ -1,11 +1,11 @@
 <?php include_once 'includes/header.php';?>
 
-<div class="jumbotron">
+<div class="jumbotron grad_div">
   <h1 class="display-4">Welcome to my Portfolio!</h1>
   <p class="lead">Here's where I like to show off some of the great work I can do.</p>
   <hr class="my-4">
   <p>To connect with me and talk about what we can do together, find my info after the jump below:</p>
-  <a class="btn btn-primary btn-lg" href="https://hrsvrt.ca/" role="button">Connect with Me</a>
+  <a class="btn btn-dark btn-lg" href="https://hrsvrt.ca/" role="button">Connect with Me</a>
 </div>
 
 <div class="container-fluid">
