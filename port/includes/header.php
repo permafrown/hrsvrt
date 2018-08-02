99834e6e5e3b604febfb344941ed351b0931eac7
permafrown <bjoelh@gmail.com>
2018-08-02 12:18:35 -0400

    adding custom css and js files

--- port/includes/header.php
+++ port/includes/header.php
@@ -6,10 +6,12 @@
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <!-- Bootstrap v4.1.3 -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Main Custom CSS -->
+      <link rel="stylesheet" href="/css/master.css">
     <title>portfolio | hrsvrt.ca</title>
   </head>
 <body>
   <div class="container-fluid">
-    <h1>portfolio <i class="fas fa-circle-notch"></i> hrsvrt.ca</h1>
-    <p>frealz again</p>
+    <h1>portfolio<i class="fas fa-circle-notch"></i>hrsvrt.ca</h1>
+    <h2>take a look at my awesome portfolio of fun stuffs!</h2>
   </div>
